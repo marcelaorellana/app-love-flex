678ac2369fc97c49681bd3e591e89def605557e2
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-06-02 14:10:52 -0400

    intento de hacer modal

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("proImg"));
+	console.log(boxes);
+	var modal = document.getElementById("modalId");
+	var bodyModal, close, img;
 
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			
+			console.log(box);
+			modal.innerHTML = "";
+
+			imgs = document.createElement("div");
+			imgs = box.innerHTML;
+			modal.appendChild(imgs);
+			
+			//modal.innerHTML = box.innerHTML;			
+			
+			modal.classList.remove("hide");
+
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			close.appendChild(img);
+			modal.appendChild(close);
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
